e395adf7e1014b14fcd010ec82404b33980e6cb5
Luiz Fernando <lf0821944@gmail.com>
2025-03-01 09:59:21 -0300

    configurando o Docke-compose

--- scripts/commands.sh
+++ scripts/commands.sh
@@ -3,11 +3,12 @@
 #O shell irá encerrar a execução do script se algum comando falhar
 
 while ! nc -z $POSTGRES_HOST $POSTGRES_PORT; do
-  echo "waiting for Postgres Database Startup($POSTGRES_HOST:$POSTGRES_PORT)..."
-  sleep 0.1
+  echo "🟡 Waiting for Postgres Database Startup ($POSTGRES_HOST $POSTGRES_PORT) ..."
+  sleep 2
 done
 
 echo "Postgres Database Started Successfully($POSTGRES_HOST:$POSTGRES_PORT)" 
-Python manage.py collectstatic
-Python manage.py migrate
-Python manage.py runserver
+python manage.py collectstatic --noinput
+python manage.py makemigrations --noinput
+python manage.py migrate --noinput
+python manage.py runserver 0.0.0.0:8000
