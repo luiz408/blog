876a6336470296451318bba835c21f2ff09b6fca
Luiz Fernando <lf0821944@gmail.com>
2025-03-01 10:31:06 -0300

    comandos docker

--- scripts/commands.sh
+++ scripts/commands.sh
@@ -2,13 +2,7 @@
 
 #O shell irá encerrar a execução do script se algum comando falhar
 
-while ! nc -z $POSTGRES_HOST $POSTGRES_PORT; do
-  echo "🟡 Waiting for Postgres Database Startup ($POSTGRES_HOST $POSTGRES_PORT) ..."
-  sleep 2
-done
-
-echo "Postgres Database Started Successfully($POSTGRES_HOST:$POSTGRES_PORT)" 
-python manage.py collectstatic --noinput
-python manage.py makemigrations --noinput
-python manage.py migrate --noinput
-python manage.py runserver 0.0.0.0:8000
+wait_psql.sh
+collectstatic.sh
+migrate.sh
+runserver.sh
