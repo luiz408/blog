709aac99649e731bfb29a90bb32f1eacc72db0f4
Luiz Fernando <lf0821944@gmail.com>
2025-03-03 17:17:21 -0300

    criando app blog

--- scripts/commands.sh
+++ scripts/commands.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-#O shell irá encerrar a execução do script se algum comando falhar
+# O shell irá encerrar a execução do script quando um comando falhar
+set -e
 
 wait_psql.sh
 collectstatic.sh
