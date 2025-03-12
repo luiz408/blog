3bbd7b74735838eb85c72970b95a582a83164cf2
Luiz Fernando <lf0821944@gmail.com>
2025-03-12 09:38:07 -0300

    otimização de imagens

--- djangoapp/site_setup/admin.py
+++ djangoapp/site_setup/admin.py
@@ -1,7 +1,5 @@
-from blog.models import Category, Page, Tag
+from blog.models import Category, Page, Post, Tag
 from django.contrib import admin
-
-
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
     list_display = 'id', 'name', 'slug',
@@ -22,8 +20,6 @@ class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {
         "slug": ('name',),
     }
-
-
 @admin.register(Page)
 class PageAdmin(admin.ModelAdmin):
     list_display = 'id', 'title', 'is_published',
@@ -36,3 +32,25 @@ class PageAdmin(admin.ModelAdmin):
     prepopulated_fields = {
         "slug": ('title',),
     }
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = 'id', 'title', 'is_published',  'created_by',
+    list_display_links = 'title',
+    search_fields = 'id', 'slug', 'title', 'excerpt', 'content',
+    list_per_page = 50
+    list_filter = 'category', 'is_published',
+    list_editable = 'is_published',
+    ordering = '-id',
+    readonly_fields = 'created_at', 'updated_at', 'created_by', 'updated_by',
+    prepopulated_fields = {
+        "slug": ('title',),
+    }
+    autocomplete_fields = 'tags', 'category',
+
+    def save_model(self, request, obj, form, change):
+        if change:
+            obj.updated_by = request.user  # type: ignore
+        else:
+            obj.created_by = request.user  # type: ignore
+
+        obj.save()
