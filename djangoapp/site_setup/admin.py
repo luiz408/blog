8304d44a8bf7cddb6282f3c4be2f5daf9215ee23
Luiz Fernando <lf0821944@gmail.com>
2025-03-16 11:01:41 -0300

    get_absolute_URL

--- djangoapp/site_setup/admin.py
+++ djangoapp/site_setup/admin.py
@@ -1,8 +1,10 @@
 from blog.models import Category, Page, Post, Tag
 from django.contrib import admin
-from django_summernote.admin import SummernoteModelAdmin
-
-
+from django.urls import reverse
+from django.utils.safestring import mark_safe
+from django_summernote.admin import SummernoteModelAdmin # type: ignore
+ 
+ 
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
     list_display = 'id', 'name', 'slug',
@@ -13,6 +15,8 @@ class TagAdmin(admin.ModelAdmin):
     prepopulated_fields = {
         "slug": ('name',),
     }
+ 
+ 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = 'id', 'name', 'slug',
@@ -23,8 +27,8 @@ class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {
         "slug": ('name',),
     }
-
-
+ 
+ 
 @admin.register(Page)
 class PageAdmin(SummernoteModelAdmin):
     summernote_fields = ('content',)
@@ -38,8 +42,8 @@ class PageAdmin(SummernoteModelAdmin):
     prepopulated_fields = {
         "slug": ('title',),
     }
-
-
+ 
+ 
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
     summernote_fields = ('content',)
@@ -50,14 +54,30 @@ class PostAdmin(SummernoteModelAdmin):
     list_filter = 'category', 'is_published',
     list_editable = 'is_published',
     ordering = '-id',
-    readonly_fields = 'created_at', 'updated_at', 'created_by', 'updated_by',
+    readonly_fields = (
+        'created_at', 'updated_at', 'created_by', 'updated_by',
+        'link',
+    )
     prepopulated_fields = {
         "slug": ('title',),
     }
     autocomplete_fields = 'tags', 'category',
+ 
+    def link(self, obj):
+        if not obj.pk:
+            return '-'
+ 
+        url_do_post = obj.get_absolute_url()
+        safe_link = mark_safe(
+            f'<a target="_blank" href="{url_do_post}">Ver post</a>'
+        )
+ 
+        return safe_link
+ 
     def save_model(self, request, obj, form, change):
         if change:
             obj.updated_by = request.user  # type: ignore
         else:
             obj.created_by = request.user  # type: ignore
+ 
         obj.save()
