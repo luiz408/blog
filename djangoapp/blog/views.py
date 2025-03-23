1bf9b335dffbf1afaf23e7fb09bc3e3a40f630a4
Luiz Fernando <lf0821944@gmail.com>
2025-03-23 15:38:27 -0300

    category para listview

--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -1,4 +1,5 @@
 from typing import Any
+
 from blog.models import Page, Post
 from django.contrib.auth.models import User
 from django.core.paginator import Paginator
@@ -10,33 +11,41 @@ from django.views.generic import ListView
 
 PER_PAGE = 9
 
+
 class PostListView(ListView):
     template_name = 'blog/pages/index.html'
     context_object_name = 'posts'
     paginate_by = PER_PAGE
     queryset = Post.objects.get_published() # type: ignore
 
-
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+
         context.update({
             'page_title': 'Home - ',
         })
+
         return context
 
 
 def created_by(request, author_pk):
     user = User.objects.filter(pk=author_pk).first()
+
     if user is None:
         raise Http404()
-    posts = Post.objects.get_published().filter(created_by__pk=author_pk) # type: ignore
+
+    posts = Post.objects.get_published()\ # type: ignore
+        .filter(created_by__pk=author_pk)  # type: ignore
     user_full_name = user.username
+
     if user.first_name:
         user_full_name = f'{user.first_name} {user.last_name}'
     page_title = 'Posts de ' + user_full_name + ' - '
+
     paginator = Paginator(posts, PER_PAGE)
     page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
+
     return render(
         request,
         'blog/pages/index.html',
@@ -45,6 +54,8 @@ def created_by(request, author_pk):
             'page_title': page_title,
         }
     )
+
+
 class CreatedByListView(PostListView):
     def __init__(self, **kwargs: Any) -> None:
         super().__init__(**kwargs)
@@ -83,14 +94,41 @@ class CreatedByListView(PostListView):
         })
 
         return super().get(request, *args, **kwargs)
+
+
+class CategoryListView(PostListView):
+    allow_empty = False
+
+    def get_queryset(self) -> QuerySet[Any]:
+        return super().get_queryset().filter(
+            category__slug=self.kwargs.get('slug')
+        )
+
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        page_title = (
+            f'{self.object_list[0].category.name}'  # type: ignore
+            ' - Categoria - '
+        )
+        ctx.update({
+            'page_title': page_title,
+        })
+        return ctx
+
+
 def category(request, slug):
-    posts = Post.objects.get_published().filter(category__slug=slug) # type: ignore
+    posts = Post.objects.get_published()\  # type: ignore
+        .filter(category__slug=slug)  # type: ignore
+
     paginator = Paginator(posts, PER_PAGE)
     page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
+
     if len(page_obj) == 0:
         raise Http404()
+
     page_title = f'{page_obj[0].category.name} - Categoria - '
+
     return render(
         request,
         'blog/pages/index.html',
@@ -100,14 +138,20 @@ def category(request, slug):
         }
     )
 
+
 def tag(request, slug):
-    posts = Post.objects.get_published().filter(tags__slug=slug) # type: ignore
+    posts = Post.objects.get_published()\  # type: ignore
+        .filter(tags__slug=slug)  # type: ignore
+
     paginator = Paginator(posts, PER_PAGE)
     page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
+
     if len(page_obj) == 0:
         raise Http404()
+
     page_title = f'{page_obj[0].tags.first().name} - Tag - '
+
     return render(
         request,
         'blog/pages/index.html',
@@ -116,18 +160,22 @@ def tag(request, slug):
             'page_title': page_title,
         }
     )
+
 
 def search(request):
     search_value = request.GET.get('search', '').strip()
+
     posts = (
-        Post.objects.get_published() # type: ignore
+        Post.objects.get_published()  # type: ignore
         .filter(
             Q(title__icontains=search_value) |
             Q(excerpt__icontains=search_value) |
             Q(content__icontains=search_value)
         )[:PER_PAGE]
     )
+
     page_title = f'{search_value[:30]} - Search - '
+
     return render(
         request,
         'blog/pages/index.html',
@@ -138,6 +186,7 @@ def search(request):
         }
     )
 
+
 def page(request, slug):
     page_obj = (
         Page.objects
@@ -145,9 +194,12 @@ def page(request, slug):
         .filter(slug=slug)
         .first()
     )
+
     if page_obj is None:
         raise Http404()
+
     page_title = f'{page_obj.title} - Página - '
+
     return render(
         request,
         'blog/pages/page.html',
@@ -157,15 +209,19 @@ def page(request, slug):
         }
     )
 
+
 def post(request, slug):
     post_obj = (
-        Post.objects.get_published() # type: ignore
+        Post.objects.get_published()  # type: ignore
         .filter(slug=slug)
         .first()
     )
+
     if post_obj is None:
         raise Http404()
+
     page_title = f'{post_obj.title} - Post - '
+
     return render(
         request,
         'blog/pages/post.html',
